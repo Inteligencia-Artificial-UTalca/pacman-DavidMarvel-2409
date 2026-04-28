56664b25aedc6cb5f1490bc815e219c7c7a112af
DavidMarvel2409 <david.marvel.programer.2409@gmail.com>
2026-04-28 15:32:49 -0400

    lab de Blinky
    intenté implementar el arbol de comportamiento de blinky pero me está dando un error que aun no detecto, ya que el fantasma no se mueve. Seguiré mas tarde

--- src/BlinkyController.cpp
+++ src/BlinkyController.cpp
@@ -1,5 +1,17 @@
 #include "BlinkyController.h"
+#include "GameState.h"
+#include <limits>
 
+
+Move getOppositeMove(Move m){
+	switch(m){
+		case UP: return DOWN;
+		case DOWN: return UP;
+		case LEFT: return RIGHT;
+		case RIGHT: return LEFT;
+		default: return PASS;
+	}
+}
 
 BlinkyController::BlinkyController(std::shared_ptr<Character> character):
 	Controller(character){
@@ -8,10 +20,61 @@ BlinkyController::BlinkyController(std::shared_ptr<Character> character):
 BlinkyController::~BlinkyController() {
 
 }
+Move BlinkyController::getMove(const GameState& game){
+    int id = 0;
+    
+    auto ghostPos = game.getGhostsPos(id);
+    auto pacmanPos = game.getPacmanPos();
 
-Move
-BlinkyController::getMove(const GameState& game){
-	return PASS;	
+    Move currentDir = (Move)game.getGhostsDir(id);
+    
+    auto moves = game.getMaze().getGhostLegalMoves(ghostPos,currentDir);
 
-	
+    bool edible = game.isGhostEdible(id);
+    Move bestMove = PASS;
+    float bestValue = edible ? -1.0f : std::numeric_limits<float>::max();
+
+    if (moves.size() == 1) {
+        return moves[0];
+    }
+
+    for (Move m : moves){
+
+        if (m == PASS)
+            continue;
+
+        int nextPos = game.getMaze().getNeighbour(ghostPos, m);
+
+        if (nextPos == -1)
+            continue;
+
+        auto nextCoord = game.getMaze().getNodePos(nextPos);
+        auto pacmanCoord = game.getMaze().getNodePos(pacmanPos);
+
+        float dist = euclid2(nextCoord, pacmanCoord);
+
+        if (edible){
+            if (dist > bestValue){
+                bestValue = dist;
+                bestMove = m;
+            }
+        }
+        else{
+            if (dist < bestValue){
+                bestValue = dist;
+                bestMove = m;
+            }
+        }
+    }
+
+    if (bestMove == PASS) {
+        for (Move m : moves) {
+            if (m != PASS) {
+                bestMove = m;
+                break;
+            }
+        }
+    }
+
+    return bestMove;
 }
